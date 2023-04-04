cae22379f3a04ae3d7394bb8509f32126f549355
Jc Ayala <j.ayala.higuera@gmail.com>
2023-04-04 11:58:28 -0400

    feat: chapter 7 complete

---
+++ lib/toy_robot/command_interpreter.ex
@@ -0,0 +1,69 @@
+defmodule ToyRobot.CommandInterpreter do
+  alias ToyRobot.{Table}
+
+  @doc """
+  Interprets commands from a commands list, in preparation for runnig them.
+
+  ## Examples
+
+    iex> alias ToyRobot.CommandInterpreter
+    ToyRobot.CommandInterpreter
+    iex> commands = ["PLACE 1,2,NORTH", "MOVE", "LEFT", "RIGHT", "REPORT"]
+    ["PLACE 1,2,NORTH", "MOVE", "LEFT", "RIGHT", "REPORT"]
+    iex> commands |> CommandInterpreter.interpret()
+    [
+      {:place, %{north: 2, east: 1, facing: :north}},
+      :move,
+      :turn_left,
+      :turn_right,
+      :report
+    ]
+  """
+  def interpret(commands) do
+    commands |> Enum.map(&do_interpret/1)
+  end
+
+  # defp do_interpret("PLACE " <> rest) do
+  #   [east, north, facing] = rest |> String.split(",")
+  #   to_int = &String.to_integer/1
+
+  #   {:place,
+  #    %{
+  #      east: to_int.(east),
+  #      north: to_int.(north),
+  #      facing: facing |> String.downcase() |> String.to_atom()
+  #    }}
+  # end
+  defp do_interpret("PLACE" <> _rest = command) do
+    format = ~r/\APLACE (\d+),(\d+),(NORTH|EAST|SOUTH|WEST)\z/
+
+    case Regex.run(format, command) do
+      [_command, east, north, facing] ->
+        to_integer = &String.to_integer/1
+
+        {:place,
+         %{
+           east: to_integer.(east),
+           north: to_integer.(north),
+           facing: facing |> String.downcase() |> String.to_atom()
+         }}
+
+      nil ->
+        {:invalid, command}
+    end
+  end
+
+  # def handle({:place, placement}) do
+  #   %Table{
+  #     north_boundary: 4,
+  #     east_boundary: 4
+  #   }
+  #   |> Simulation.place(placement)
+  # end
+
+  defp do_interpret("MOVE"), do: :move
+  defp do_interpret("LEFT"), do: :turn_left
+  defp do_interpret("RIGHT"), do: :turn_right
+  defp do_interpret("REPORT"), do: :report
+  defp do_interpret(command), do: {:invalid, command}
+end
---
+++ robot.exs
@@ -0,0 +1,4 @@
+File.stream!("command.txt")
+|> Enum.map(&String.trim/1)
+|> ToyRobot.CommandInterpreter.interpret()
+|> ToyRobot.CommandRunner.run()
